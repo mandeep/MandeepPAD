ff3c3c555ed6b5060e5378297ada26a335316591
mandeepbhutani <bhutanimandeep@gmail.com>
2016-07-01 18:04:32 -0500

    Added text edit class along with menu bar and status bar

--- application.py
+++ application.py
@@ -1,13 +1,36 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QTextEdit
+from PyQt5.QtWidgets import QMainWindow, QApplication, QTextEdit
 
 
-application = QApplication(sys.argv)
+class TextEditor(QMainWindow):
 
-window = QTextEdit()
-window.resize(1024, 768)
-window.move(400, 200)
-window.setWindowTitle('MandeepPAD')
-window.show()
+    def __init__(self, parent=None):
+        QMainWindow.__init__(self, parent)
+        self.initUI()
 
-sys.exit(application.exec_())
+    def initUI(self):
+        self.text = QTextEdit(self)
+        self.setCentralWidget(self.text)
+
+        self.setGeometry(100, 100, 1024, 768)
+        self.setWindowTitle('MandeepPAD')
+        self.move(400, 200)
+
+        self.initMenuBar()
+        self.statusbar = self.statusBar()
+
+    def initMenuBar(self):
+        menubar = self.menuBar()
+        file = menubar.addMenu('File')
+        edit = menubar.addMenu('Edit')
+        preferences = menubar.addMenu('Preferences')
+        help_menu = menubar.addMenu('Help')
+
+
+def main():
+    application = QApplication(sys.argv)
+    window = TextEditor()
+    window.show()
+    sys.exit(application.exec_())
+
+main()
