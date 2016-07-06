272badd311269fc56561317251d6ddf8ef1d7be0
mandeepbhutani <bhutanimandeep@gmail.com>
2016-07-05 21:27:38 -0500

    Tests for MandeepPAD

---
+++ tests.py
@@ -0,0 +1,22 @@
+import os
+import sys
+import mpad
+import pytest
+from PyQt5.QtGui import QIcon
+from PyQt5.QtPrintSupport import QPrintDialog
+from PyQt5.QtWidgets import (QMainWindow, QApplication, QTextEdit, QAction,
+                             QFileDialog, QFontDialog, QMessageBox, QInputDialog)
+
+
+class TestClass:
+
+    def setup(self):
+        appplication = QApplication(sys.argv)
+        self.window = mpad.TextEditor()
+
+    def test_window_name(self):
+        assert self.window.windowTitle() == 'MandeepPAD'
+
+    def test_geometry(self):
+        assert self.window.height() == 768
+        assert self.window.width() == 1024
