d3b45a832a72dee286c05a706be7814ac729b536
mandeepbhutani <bhutanimandeep@gmail.com>
2016-07-14 16:49:29 -0500

    Updated to include pyqt5 if python version greater than 3.5

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
+import sys
 from setuptools import setup, find_packages
+
+if sys.version_info >= (3, 5):
+    dependencies = ['pyqt5', 'arrow']
+else:
+    dependencies = ['arrow']
 
 setup(name='MandeepPAD',
       version='0.0.1',
       author='Mandeep Bhutani',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=[
-        'arrow'
-      ],
+      install_requires=dependencies,
       entry_points='''
         [console_scripts]
         MandeepPAD=mpad:main
