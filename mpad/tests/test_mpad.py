96144da8716402da9bf6cc99350416856dabc7e6
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-12 01:13:05 -0500

    More file menu item tests

--- mpad/tests/test_mpad.py
+++ mpad/tests/test_mpad.py
@@ -1,5 +1,5 @@
 from mpad import editor
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, QTimer
 from PyQt5.QtPrintSupport import QPrintDialog
 from PyQt5.QtWidgets import (QMainWindow, QApplication, QTextEdit, QAction,
                              QFileDialog, QFontDialog, QMessageBox, QInputDialog, QWidget)
@@ -31,15 +31,13 @@ class TestClass:
         assert self.editor.text.tabStopWidth() == 50
 
     def test_bars(self):
-        """
-        Tests whether the widget contains a status bar and menu bar.
+        """Tests whether the widget contains a status bar and menu bar.
         """
         assert self.editor.statusBar().isVisible()
         assert self.editor.menuBar().isVisible()
 
     def test_menu_bar(self):
-        """
-        Tests whether the menu items appear on the menu bar.
+        """Tests whether the menu items appear on the menu bar.
         """
         assert self.editor.file.isEnabled()
         assert self.editor.edit.isEnabled()
@@ -48,20 +46,61 @@ class TestClass:
         assert self.editor.view.isEnabled()
         assert self.editor.help_option.isEnabled()
 
-    def test_file_menu_items(self, qtbot):
+    def test_new_file(self, qtbot):
+        """Qtbot simulates clicks inside the text editor window to ensure
+        that a new window opens when New File is selected.
         """
-        Qtbot simulates mouse clicks inside the text editor window to ensure
-        that the items on the menu bar are clickable.
-        """
-        # Test to see if a new window opens when New File is selected
         qtbot.mouseClick(self.editor.file, Qt.LeftButton)
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
         qtbot.keyClick(self.editor.file, Qt.Key_Enter)
         assert self.editor.new_window.windowTitle() == 'MandeepPAD'
 
-        # Test to see if an open dialog opens when Open File is selected
+    def test_open_file(self, qtbot, mock):
+        """Qtbot simulates clicks inside the text editor window to ensure
+        that an open file dialog opens when Open File is selected."""
         qtbot.mouseClick(self.editor.file, Qt.LeftButton)
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        mock.patch.object(QFileDialog, 'getOpenFileName', return_value=('', ''))
+        self.editor.open_file()
+
+    def test_save_file(self, qtbot, mock):
+        """Qtbot simulates clicks inside the text editor window to ensure
+        that a save file dialog opens when Save File is selected."""
+        qtbot.mouseClick(self.editor.file, Qt.LeftButton)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        mock.patch.object(QFileDialog, 'getSaveFileName', return_value=('', ''))
+        self.editor.save_file()
+
+    def test_print_file(self, qtbot, mock):
+        """Qtbot simulates clicks inside the text editor window to ensure
+        that a save file dialog opens when Save File is selected."""
+        qtbot.mouseClick(self.editor.file, Qt.LeftButton)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        
+        dialog = QPrintDialog()
+        qtbot.addWidget(dialog)
+
+        def interact():
+            dialog.close()
+
+        QTimer.singleShot(500, interact)
+
+    def test_quit_application(self, qtbot, monkeypatch):
+        exit_calls = []
+        monkeypatch.setattr(QApplication, 'quit', lambda: exit_calls.append(1))
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Down)
+        qtbot.keyClick(self.editor.file, Qt.Key_Enter)
+        assert exit_calls == [1]
 
     def test_menu_items(self, qtbot):
         qtbot.mouseClick(self.editor.edit, Qt.LeftButton)
