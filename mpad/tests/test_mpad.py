96e6b0c370cb0538aebc1b6db56a5a0f020094ee
mandeepbhutani <bhutanimandeep@gmail.com>
2016-08-15 01:28:48 -0500

    Added tests for more menu items

--- mpad/tests/test_mpad.py
+++ mpad/tests/test_mpad.py
@@ -53,7 +53,6 @@ class TestClass:
         qtbot.mouseClick(self.editor.file, Qt.LeftButton)
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
         qtbot.keyClick(self.editor.file, Qt.Key_Enter)
-        assert self.editor.new_window.windowTitle() == 'MandeepPAD'
 
     def test_open_file(self, qtbot, mock):
         """Qtbot simulates clicks inside the text editor window to ensure
@@ -61,7 +60,7 @@ class TestClass:
         qtbot.mouseClick(self.editor.file, Qt.LeftButton)
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
-        mock.patch.object(QFileDialog, 'getOpenFileName', return_value=('', ''))
+        mock.patch.object(QFileDialog, 'getOpenFileName', return_value=('.travis.yml', '*.txt'))
         self.editor.open_file()
 
     def test_save_file(self, qtbot, mock):
@@ -71,7 +70,7 @@ class TestClass:
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
-        mock.patch.object(QFileDialog, 'getSaveFileName', return_value=('', ''))
+        mock.patch.object(QFileDialog, 'getSaveFileName', return_value=('.travis.yml', '*.txt'))
         self.editor.save_file()
 
     def test_print_file(self, qtbot, mock):
@@ -82,14 +81,8 @@ class TestClass:
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
         qtbot.keyClick(self.editor.file, Qt.Key_Down)
-        
-        dialog = QPrintDialog()
-        qtbot.addWidget(dialog)
-
-        def interact():
-            dialog.close()
-
-        QTimer.singleShot(500, interact)
+        mock.patch.object(QPrintDialog, 'exec_', return_value='accept')
+        qtbot.keyClick(self.editor.file, Qt.Key_Enter)
 
     def test_quit_application(self, qtbot, monkeypatch):
         """Qtbot scolls through menu items and monkeypatch intercepts the quit
@@ -134,9 +127,72 @@ class TestClass:
         cursor.select(3)
         assert cursor.selectedText() == sentence
 
-    def test_menu_items(self, qtbot):
+    def test_delete_action(self, qtbot):
+        """Inserts text into the text area then qtbot navigates to Delete
+        and selects it.
+        """
+        sentence = "Go get help, Dog."
+        cursor = self.editor.text.textCursor()
+        cursor.insertText(sentence)
         qtbot.mouseClick(self.editor.edit, Qt.LeftButton)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Enter)
+
+    def test_select_all(self, qtbot):
+        """Inserts text into the text area then qtbot navigates to the Select
+        All item and seelcts it."""
+        sentence = "Oh, the boy. Bring me the boy."
+        cursor = self.editor.text.textCursor()
+        cursor.insertText(sentence)
+        qtbot.mouseClick(self.editor.edit, Qt.LeftButton)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Enter)
+
+    def test_font_dialog(self, qtbot, mock):
+        """Qtbot opens the format menu and selects the Change Font item."""
         qtbot.mouseClick(self.editor.form, Qt.LeftButton)
+        qtbot.keyClick(self.editor.form, Qt.Key_Down)
+        mock.patch.object(QFontDialog, 'getFont', return_value=('', ''))
+        qtbot.keyClick(self.editor.form, Qt.Key_Enter)
+
+    def test_bold_font(self, qtbot):
+        """Qtbot opens the format menu and selects the Bold item."""
+        qtbot.mouseClick(self.editor.form, Qt.LeftButton)
+        qtbot.keyClick(self.editor.form, Qt.Key_Down)
+        qtbot.keyClick(self.editor.form, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Enter)
+
+    def test_italic_font(self, qtbot):
+        """Qtbot opens the format menu and selects the Italicize item."""
+        qtbot.mouseClick(self.editor.form, Qt.LeftButton)
+        qtbot.keyClick(self.editor.form, Qt.Key_Down)
+        qtbot.keyClick(self.editor.form, Qt.Key_Down)
+        qtbot.keyClick(self.editor.form, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Enter)
+
+    def test_underline_font(self, qtbot):
+        """Qtbot opens the format menu and selects the Underline item."""
+        qtbot.mouseClick(self.editor.form, Qt.LeftButton)
+        qtbot.keyClick(self.editor.form, Qt.Key_Down)
+        qtbot.keyClick(self.editor.form, Qt.Key_Down)
+        qtbot.keyClick(self.editor.form, Qt.Key_Down)
+        qtbot.keyClick(self.editor.form, Qt.Key_Down)
+        qtbot.keyClick(self.editor.edit, Qt.Key_Enter)
+
+    def test_text_search(self, qtbot, mock):
+        """Qtbot opens the tools menu and selects the Find text item."""
         qtbot.mouseClick(self.editor.tools, Qt.LeftButton)
-        qtbot.mouseClick(self.editor.view, Qt.LeftButton)
-        qtbot.mouseClick(self.editor.help_option, Qt.LeftButton)
+        qtbot.keyClick(self.editor.tools, Qt.Key_Down)
+        mock.patch.object(QInputDialog, 'getText', return_value=('Hi!', True))
+        qtbot.keyClick(self.editor.tools, Qt.Key_Enter)
