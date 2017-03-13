253839a33f3a0786f338dd8a5e2014f784a98dbb
mandeep <mandeep@users.noreply.github.com>
2017-03-13 15:16:20 -0500

    Code cleanup and fixed toolbar_visibility test

--- mpad/tests/test_mpad.py
+++ mpad/tests/test_mpad.py
@@ -7,6 +7,7 @@ from PyQt5.QtWidgets import QApplication, QFileDialog, QFontDialog, QMessageBox,
 
 @pytest.fixture
 def window(qtbot):
+    """Text editor fixture to be passed to the tests."""
     new_window = editor.TextEditor()
     qtbot.add_widget(new_window)
     new_window.show()
@@ -14,19 +15,19 @@ def window(qtbot):
 
 
 def test_window(window):
-    """Tests the window title and that the window icon is visible."""
+    """Test the window title and that the window icon is visible."""
     assert window.windowTitle() == 'MandeepPAD'
     assert window.windowIcon().isNull() is False
 
 
 def test_geometry(window):
-    """Tests the default window geometry."""
+    """Test the default window geometry."""
     assert window.width() == 1024
     assert window.height() == 768
 
 
 def test_text_area(qtbot, window):
-    """Tests that the text is being inserted into the text area."""
+    """Test that the text is being inserted into the text area."""
     sentence = 'They gave my red hat to the donkey.'
     cursor = window.text.textCursor()
     qtbot.add_widget(cursor)
@@ -37,15 +38,13 @@ def test_text_area(qtbot, window):
 
 
 def test_bars(window):
-    """Tests whether the widget contains a status bar and menu bar.
-    """
+    """Test whether the widget contains a status bar and menu bar."""
     assert window.statusBar().isVisible()
     assert window.menuBar().isVisible()
 
 
 def test_menu_bar(window):
-    """Tests whether the menu items appear on the menu bar.
-    """
+    """Test whether the menu items appear on the menu bar."""
     assert window.file.isEnabled()
     assert window.edit.isEnabled()
     assert window.form.isEnabled()
@@ -55,7 +54,9 @@ def test_menu_bar(window):
 
 
 def test_new_file(qtbot, window):
-    """Qtbot simulates clicks inside the text editor window to ensure
+    """Test the 'New File' item on the File submenu.
+
+    Qtbot simulates clicks inside the text editor window to ensure
     that a new window opens when New File is selected.
     """
     qtbot.mouseClick(window.file, Qt.LeftButton)
@@ -64,7 +65,9 @@ def test_new_file(qtbot, window):
 
 
 def test_open_file(qtbot, mock, window):
-    """Qtbot simulates clicks inside the text editor window to ensure
+    """Test the 'Open File' item on the File submenu.
+
+    Qtbot simulates clicks inside the text editor window to ensure
     that an open file dialog opens when Open File is selected."""
     qtbot.mouseClick(window.file, Qt.LeftButton)
     qtbot.keyClick(window.file, Qt.Key_Down)
@@ -74,7 +77,9 @@ def test_open_file(qtbot, mock, window):
 
 
 def test_save_file(qtbot, mock, window):
-    """Qtbot simulates clicks inside the text editor window to ensure
+    """Test the 'Save File' item on the File submenu.
+
+    Qtbot simulates clicks inside the text editor window to ensure
     that a save file dialog opens when Save File is selected."""
     qtbot.mouseClick(window.file, Qt.LeftButton)
     qtbot.keyClick(window.file, Qt.Key_Down)
@@ -85,7 +90,9 @@ def test_save_file(qtbot, mock, window):
 
 
 def test_print_file(qtbot, mock, window):
-    """Qtbot simulates clicks inside the text editor window to ensure
+    """Test the 'Print' item on the File submenu.
+
+    Qtbot simulates clicks inside the text editor window to ensure
     that a save file dialog opens when Save File is selected."""
     qtbot.mouseClick(window.file, Qt.LeftButton)
     qtbot.keyClick(window.file, Qt.Key_Down)
@@ -97,7 +104,9 @@ def test_print_file(qtbot, mock, window):
 
 
 def test_quit_application(qtbot, monkeypatch, window):
-    """Qtbot scolls through menu items and monkeypatch intercepts the quit
+    """Test the 'Quit' item on the File submenu.
+
+    Qtbot scolls through menu items and monkeypatch intercepts the quit
     application call prior to it being called."""
     exit_calls = []
     monkeypatch.setattr(QApplication, 'quit', lambda: exit_calls.append(1))
@@ -111,7 +120,9 @@ def test_quit_application(qtbot, monkeypatch, window):
 
 
 def test_undo_action(qtbot, window):
-    """Inserts text into the text area then qtbot navigates to Undo
+    """Test the 'Undo' item on the Edit submenu.
+
+    Inserts text into the text area then qtbot navigates to Undo
     and selects it.
     """
     sentence = "My back. There goes my back again."
@@ -126,8 +137,10 @@ def test_undo_action(qtbot, window):
 
 
 def test_redo_action(qtbot, window):
-    """Inserts text into the text area then qtbot navigates to Undo
-    and selects it. Qtbot  then navigates to the redo action and
+    """Test the 'Redo' item on the Edit submenu.
+
+    Inserts text into the text area then qtbot navigates to Undo
+    and selects it. Qtbot then navigates to the redo action and
     selects it.
     """
     sentence = "Oh, hello, Principal Skinner. I'd get up, but the boy crippled me."
@@ -145,7 +158,9 @@ def test_redo_action(qtbot, window):
 
 
 def test_delete_action(qtbot, window):
-    """Inserts text into the text area then qtbot navigates to Delete
+    """Test the 'Delete' item on the Edit submenu.
+
+    Inserts text into the text area then qtbot navigates to Delete
     and selects it.
     """
     sentence = "Go get help, Dog."
@@ -163,7 +178,9 @@ def test_delete_action(qtbot, window):
 
 
 def test_select_all(qtbot, window):
-    """Inserts text into the text area then qtbot navigates to the Select
+    """Test the 'Delete' item on the Edit submenu.
+
+    Inserts text into the text area then qtbot navigates to the Select
     All item and seelcts it."""
     sentence = "Oh, the boy. Bring me the boy."
     cursor = window.text.textCursor()
@@ -181,42 +198,19 @@ def test_select_all(qtbot, window):
 
 
 def test_font_dialog(qtbot, mock, window):
-    """Qtbot opens the format menu and selects the Change Font item."""
+    """Test the 'Font' item on the Format submenu.
+
+    Qtbot opens the format menu and selects the Change Font item."""
     qtbot.mouseClick(window.form, Qt.LeftButton)
     qtbot.keyClick(window.form, Qt.Key_Down)
     mock.patch.object(QFontDialog, 'getFont', return_value=('', ''))
     qtbot.keyClick(window.form, Qt.Key_Enter)
 
 
-def test_bold_font(qtbot, window):
-    """Qtbot opens the format menu and selects the Bold item."""
-    qtbot.mouseClick(window.form, Qt.LeftButton)
-    qtbot.keyClick(window.form, Qt.Key_Down)
-    qtbot.keyClick(window.form, Qt.Key_Down)
-    qtbot.keyClick(window.edit, Qt.Key_Enter)
-
-
-def test_italic_font(qtbot, window):
-    """Qtbot opens the format menu and selects the Italicize item."""
-    qtbot.mouseClick(window.form, Qt.LeftButton)
-    qtbot.keyClick(window.form, Qt.Key_Down)
-    qtbot.keyClick(window.form, Qt.Key_Down)
-    qtbot.keyClick(window.form, Qt.Key_Down)
-    qtbot.keyClick(window.edit, Qt.Key_Enter)
-
-
-def test_underline_font(qtbot, window):
-    """Qtbot opens the format menu and selects the Underline item."""
-    qtbot.mouseClick(window.form, Qt.LeftButton)
-    qtbot.keyClick(window.form, Qt.Key_Down)
-    qtbot.keyClick(window.form, Qt.Key_Down)
-    qtbot.keyClick(window.form, Qt.Key_Down)
-    qtbot.keyClick(window.form, Qt.Key_Down)
-    qtbot.keyClick(window.edit, Qt.Key_Enter)
-
-
 def test_text_search(qtbot, mock, window):
-    """Qtbot opens the tools menu and selects the Find text item."""
+    """Test the 'Find Text' item on the Tools submenu.
+
+    Qtbot opens the tools menu and selects the Find text item."""
     qtbot.mouseClick(window.tools, Qt.LeftButton)
     qtbot.keyClick(window.tools, Qt.Key_Down)
     mock.patch.object(QInputDialog, 'getText', return_value=('Hi!', True))
@@ -224,7 +218,9 @@ def test_text_search(qtbot, mock, window):
 
 
 def test_date(qtbot, window):
-    """Qtbot opens the tools menu and selects the Insert Date item."""
+    """Test the 'Insert Date/Time' item on the Tools submenu.
+
+    Qtbot opens the tools menu and selects the Insert Date item."""
     cursor = window.text.textCursor()
     qtbot.add_widget(cursor)
     qtbot.mouseClick(window.tools, Qt.LeftButton)
@@ -237,7 +233,9 @@ def test_date(qtbot, window):
 
 
 def test_menu_bar_visibility(qtbot, window):
-    """Qtbot opens the view menu and selects the Hide Menu Bar item."""
+    """Test the 'Hide Menubar' item on the View submenu.
+
+    Qtbot opens the view menu and selects the Hide Menubar item."""
     qtbot.mouseClick(window.view, Qt.LeftButton)
     qtbot.keyClick(window.view, Qt.Key_Down)
     qtbot.keyClick(window.view, Qt.Key_Enter)
@@ -245,16 +243,20 @@ def test_menu_bar_visibility(qtbot, window):
 
 
 def test_tool_bar_visibility(qtbot, window):
-    """Qtbot opens the view menu and selects the Hide Tool Bar item."""
+    """Test the 'Show Toolbar' item on the View submenu.
+
+    Qtbot opens the view menu and selects the Show Toolbar item."""
     qtbot.mouseClick(window.view, Qt.LeftButton)
     qtbot.keyClick(window.view, Qt.Key_Down)
     qtbot.keyClick(window.view, Qt.Key_Down)
     qtbot.keyClick(window.view, Qt.Key_Enter)
-    assert window.toolbar.isVisible() is False
+    assert window.toolbar.isVisible() is True
 
 
 def test_status_bar_visibility(qtbot, window):
-    """Qtbot opens the view menu and selects the Hide Status Bar item."""
+    """Test the 'Hide Statusbar' item on the View submenu.
+
+    Qtbot opens the view menu and selects the Hide Statusbar item."""
     qtbot.mouseClick(window.view, Qt.LeftButton)
     qtbot.keyClick(window.view, Qt.Key_Down)
     qtbot.keyClick(window.view, Qt.Key_Down)
@@ -264,7 +266,9 @@ def test_status_bar_visibility(qtbot, window):
 
 
 def test_about(qtbot, mock, window):
-    """Qtbot opens the help menu and mock executes the about message box."""
+    """Test the 'About' item on the Help submenu.
+
+    Qtbot opens the help menu and mock executes the about message box."""
     qtbot.mouseClick(window.help_option, Qt.LeftButton)
     qtbot.keyClick(window.help_option, Qt.Key_Down)
     mock.patch.object(QMessageBox, 'exec_', return_value='accept')
